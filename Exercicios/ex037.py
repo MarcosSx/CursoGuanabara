6d09632c869627b229eeaa9be760d35c3aaa8775
Marcos <marcosxavier03@gmail.com>
2021-10-28 14:29:25 -0300

    adicionando exercicio 39

--- Exercicios/ex037.py
+++ Exercicios/ex037.py
@@ -1,6 +1,6 @@
 # Exercício Python 037: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher
 # qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
-#todo
+# todo converter para bases
 numero = float(input('Digite um numero para ser convertido\n>'))
 print('Você quer converter o numero {:.0f} para:'.format(numero))
 opcoes = int(input('1 - BINARIO\n2 - OCTAL\n3 - HEXADECIMAL\n>'))
--- Exercicios/ex039.py
+++ Exercicios/ex039.py
@@ -1,10 +1,29 @@
 # Exercício Python 039: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade,
 # se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do
 # alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
-#todo
-import datetime
-diaNascimento = int(input('Digite o dia que nasceu'))
-mesNascimento = int(input('Digite o mes que nasceu'))
-anoNascimento = int(input('Digite o ano que nasceu'))
-print('Voce nasceu em {}/{}/{}'.format(diaNascimento, mesNascimento, anoNascimento))
-print(datetime.date.strftime())
+
+from datetime import date
+
+anoNas = int(input('Digite o ano que nasceu?\n'))
+idade = date.today().year - anoNas
+
+if idade > 18:
+    alistou = str(input('Ja se alistou-se no EB (S) ou (N)?\n')).upper()
+
+    if alistou == 'N':
+        passou = idade - 18
+        print('Sua idade é {}\nVoce ja passou da idade para se alistar.\nEstá {} ano(s) atrasado'.format(idade, passou))
+        print('Seu ano de alistamento foi em {}'.format(date.today().year - passou))
+
+    elif alistou == 'S':
+        print('Obrigado pela informação')
+
+    else:
+        print('Opção invalida')
+
+elif idade == 18:
+    print('Sua idade é {}\nVoce deve alistar-se este ano'.format(idade))
+
+else:
+    falta = 18 - idade
+    print('Sua idade é {}\nVoce deve se alistar daqui {} anos em {}'.format(idade, falta, falta + date.today().year))
