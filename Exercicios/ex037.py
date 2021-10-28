0b568f95964694dc11c929f193592db780136fbb
Marcos <marcosxavier03@gmail.com>
2021-10-28 14:54:26 -0300

    adicionando exercicio 37

--- Exercicios/ex037.py
+++ Exercicios/ex037.py
@@ -1,15 +1,15 @@
 # Exercício Python 037: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher
 # qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
-# todo converter para bases
-numero = float(input('Digite um numero para ser convertido\n>'))
+
+numero = int(input('Digite um numero para ser convertido\n>'))
 print('Você quer converter o numero {:.0f} para:'.format(numero))
 opcoes = int(input('1 - BINARIO\n2 - OCTAL\n3 - HEXADECIMAL\n>'))
-converterBin = numero // 2
+
 if opcoes == 1:
-    print('Convertendo {:.0f} para BINARIO teremos '.format(numero))
+    print('Convertendo...\nO numero {:.0f} em BINARIO é {}'.format(numero, bin(numero)[2:]))
 elif opcoes == 2:
-    print('Convertendo {:.0f} para OCTAL teremos '.format(numero))
+    print('Convertendo...\nO numero {:.0f} em OCTAL é {}'.format(numero, oct(numero)[2:]))
 elif opcoes == 3:
-    print('Convertendo {:.0f} para HEXADECIMAL teremos '.format(numero))
+    print('Convertendo...\nO numero {:.0f} em HEXADECIMAL é {}'.format(numero, hex(numero)[2:]))
 else:
     print('Opção invalida')
