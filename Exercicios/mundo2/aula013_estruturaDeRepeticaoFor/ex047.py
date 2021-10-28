f17dd28f2aad15cbb049ef0c4e0ad66ced5e1924
Marcos <marcosxavier03@gmail.com>
2021-10-28 18:19:29 -0300

    exercicio 47 concluido

--- Exercicios/mundo2/aula013_estruturaDeRepeticaoFor/ex047.py
+++ Exercicios/mundo2/aula013_estruturaDeRepeticaoFor/ex047.py
@@ -1,1 +1,5 @@
 # Exercício Python 047: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+print('Mostrando numeros pares entre 1 e 50:')
+for c in range(0, 50, 2):
+    print(c, end=' ')
