a8252f851e6382d6a3e8c4b3cbd10b49a6a940c8
Marcos <marcosxavier03@gmail.com>
2021-10-28 12:44:12 -0300

    Adicionando exercicio 41

--- Exercicios/ex041.py
+++ Exercicios/ex041.py
@@ -5,5 +5,24 @@
 # - Até 19 anos: JÚNIOR
 # - Até 25 anos: SÊNIOR
 # - Acima de 25 anos: MASTER
-#todo
 
+from datetime import date
+
+anoNasc = int(input('Insira o ano de nascimento do atleta para saber sua categoria\n>>'))
+anoAtual = date.today().year
+
+idade = anoAtual - anoNasc
+
+if anoAtual >= anoNasc >= 1900:
+    if idade <= 9:
+        print('Atleta categoria MIRIM')
+    elif idade <= 14:
+        print('Atleta categoria INFANTIL')
+    elif idade <= 19:
+        print('Atleta categoria JUNIOR')
+    elif idade <= 25:
+        print('Atleta categoria SÊNIOR')
+    else:
+        print('Atleta categoria MASTER')
+else:
+    print('Ano invalido')
