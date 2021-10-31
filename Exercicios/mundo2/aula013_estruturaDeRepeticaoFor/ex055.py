c554ab9557b6e20277f5388d57a55523adac9656
Marcos <marcosxavier03@gmail.com>
2021-10-31 03:17:58 -0300

    exercicio 55 concluido

--- Exercicios/mundo2/aula013_estruturaDeRepeticaoFor/ex055.py
+++ Exercicios/mundo2/aula013_estruturaDeRepeticaoFor/ex055.py
@@ -1,3 +1,15 @@
 # Exercício Python 055: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor
 # peso lidos.
-# todo ler pesos e dizer qual foi o maior
+maior = 0
+menor = 0
+for i in range(1, 6):
+    peso = float(input('Digite seu peso: '))
+    if i == 1:
+        maior = peso
+        menor = peso
+    if peso > maior:
+        maior = peso
+    else:
+        menor = peso
+
+print('maior {}, menor {}'.format(maior, menor))
