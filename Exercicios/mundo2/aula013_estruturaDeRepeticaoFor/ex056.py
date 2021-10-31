bc6fce2cd7033479a841d6feed0b0089bf3bb875
Marcos <marcosxavier03@gmail.com>
2021-10-31 03:46:27 -0300

    exercicio 56 concluido Exercicios da aula 13 finalizados

--- Exercicios/mundo2/aula013_estruturaDeRepeticaoFor/ex056.py
+++ Exercicios/mundo2/aula013_estruturaDeRepeticaoFor/ex056.py
@@ -1,3 +1,25 @@
 # Exercício Python 056: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre:
 # a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
-# todo ler nome idade e sexo de  4 pessoas e motrar idade media, quem é mais velho e quantidade de mulheres
+mediaIdade = 0
+somaIdade = 0
+maisVelho = 0
+nomeMaisVelho = ''
+quantMulher = 0
+for pessoa in range(1, 5):
+    print('-*-' * 15)
+    nome = str(input('Digite o nome da pessoa: '))
+    idade = float(input('Digite o idade da pessoa: '))
+    sexo = str(input('Digite o sexo da pessoa (M)/(F): '))
+    if pessoa == 1 and sexo in 'Mm':
+        maisVelho = idade
+    if sexo in 'Mm' and idade > maisVelho:
+        maisVelho = idade
+        nomeMaisVelho = nome
+    if sexo in 'Ff' and idade < 20:
+        quantMulher += 1
+    print('-*-' * 15)
+    somaIdade += idade
+mediaIdade = somaIdade / 4
+print('A meida de idade do grupo é de {}'.format(mediaIdade))
+print('O homem mais velho é {} com {} anos'.format(nomeMaisVelho, maisVelho))
+print('A quantidade de mulheres abaixo de 20 anos é de {}'.format(quantMulher))
