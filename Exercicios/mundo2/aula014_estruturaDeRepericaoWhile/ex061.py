01a745aae13ac4cf99cbbdff19f06535f349650d
Marcos <marcosxavier03@gmail.com>
2021-11-01 02:56:45 -0300

    Adicionando enunciados dos exercicios da aula 14

---
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex061.py
@@ -0,0 +1,2 @@
+# Exercício Python 061: Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros
+# termos da progressão usando a estrutura while
---
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex063.py
@@ -0,0 +1,2 @@
+# Exercício Python 063: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros
+# elementos de uma Sequência de Fibonacci.
