2c8f81e36d2aab831d205d889620e23d02030d65
Marcos <marcosxavier03@gmail.com>
2021-11-01 14:01:10 -0300

    exercicio 61 concluiudo

--- Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex061.py
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex061.py
@@ -1,2 +1,12 @@
 # Exercício Python 061: Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros
 # termos da progressão usando a estrutura while
+
+num = float(input('Digite o primeiro termo da PA: '))
+razao = float(input('Digite a razão dessa PA: '))
+print('PA = {}'.format(num), end=' → ')
+decimo = num + (10 - 1) * razao
+while num != decimo:
+    pa = num + razao
+    print(pa, end=' → ')
+    num += razao
+print('Fim')
