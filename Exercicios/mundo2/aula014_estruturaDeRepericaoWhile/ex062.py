740aadbee4c7987bcc6450b26c6fe206a6d611cc
Marcos <marcosxavier03@gmail.com>
2021-11-01 14:24:07 -0300

    exercicio 62 concluiudo

--- Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex062.py
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex062.py
@@ -1,2 +1,19 @@
 # Exercício Python 062: Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O
 # programa encerrará quando ele disser que quer mostrar 0 termos.
+
+num = float(input('Digite o primeiro termo da PA: '))
+razao = float(input('Digite a razão dessa PA: '))
+quant = int(input('Digite quantos termos deseja ver: '))
+termos = num + (quant - 1) * razao
+print('PA = {}'.format(num), end=' → ')
+while num != termos:
+    pa = num + razao
+    print(pa, end=' → ')
+    num += razao
+    if num == termos:
+        quant = int(input('\nDigite quantos mais termos deseja ver: '))
+        termos = num + (quant - 1) * razao
+        print('PA = {}'.format(num), end=' → ')
+        if quant == 0:
+            termos = num
+print('Fim')
