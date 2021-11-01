6220f2accd3be9aebef73bc543327592f0ffb1f4
Marcos <marcosxavier03@gmail.com>
2021-11-01 17:06:31 -0300

    melhoria no exercicio 60

--- Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex061.py
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex061.py
@@ -1,5 +1,18 @@
 # Exercício Python 061: Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros
 # termos da progressão usando a estrutura while
+# Resolução Guanabara
+
+print('Gerador de PA')
+print('-=' * 10)
+# primeiro = int(input('Primeiro termo: '))
+# razão = int(input('Razão da PA: '))
+# termo = primeiro
+# cont = 1
+# while cont <= 10:
+#     print('{} → '.format(termo), end='')
+#     termo += razão
+#     cont += 1
+# print('FIM')
 
 num = float(input('Digite o primeiro termo da PA: '))
 razao = float(input('Digite a razão dessa PA: '))
