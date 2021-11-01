6da0cc64c92da729ef2dec6e878691b4b95122ef
Marcos <marcosxavier03@gmail.com>
2021-11-01 10:02:17 -0300

    exercicio 57 concluido

--- Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex057.py
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex057.py
@@ -1,2 +1,10 @@
 # Exercício Python 057: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja
 # errado, peça a digitação novamente até ter um valor correto.
+
+sexo = str(input('Digite seu sexo [M/F]: ')).upper()
+while sexo != 'F' and sexo != 'M':
+    print('{} não é um sexo valido!!'.format(sexo))
+    sexo = str(input('Digite seu sexo [M/F]: ')).upper()
+    sexo = sexo
+
+print('Voce digitou {}, sexo valido'.format(sexo))
