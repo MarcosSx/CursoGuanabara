872cd4ef12af59d6f766f1ab3f57f369c46100d1
Marcos <marcosxavier03@gmail.com>
2021-11-01 12:09:46 -0300

    exercicio 59 concluiudo

--- Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex059.py
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex059.py
@@ -5,3 +5,29 @@
 # [ 4 ] novos números
 # [ 5 ] sair do programa
 # Seu programa deverá realizar a operação solicitada em cada caso.
+
+sair = 0
+num1 = float(input('Digite o 1º numero: '))
+num2 = float(input('Digite o 2º numero: '))
+while sair != 5:
+    print('-=-' * 12)
+    print('[ 1 ] somar | [ 2 ] multiplicar\n[ 3 ] maior | [ 4 ] novos números\n[ 5 ] sair do programa')
+    opcao = int(input('Digite uma opção: '))
+    if opcao != 5 and opcao < 5:
+        if opcao == 1:
+            print('[ 1 ] somar\nA soma dos numeros {} e {} é igual a {}\n'.format(num1, num2, num1 + num2))
+        elif opcao == 2:
+            print('[ 2 ] multiplicar\nMultiplicando os numeros {} e {} é igual a {}'.format(num1, num2, num1 * num2))
+        elif opcao == 3:
+            print('[ 3 ] maior\nVoce digitou os numeros {} e {} e o maior é {}'.format(num1, num2,
+                                                                                       num1 if num1 > num2 else num2))
+        elif opcao == 4:
+            print('[ 4 ] limpar números\nDigite novos numeros')
+            num1 = float(input('Digite o 1º numero: '))
+            num2 = float(input('Digite o 2º numero: '))
+        else:
+            print('Voce digitou {}\nOPÇÃO INVALIDA'.format(opcao))
+    elif opcao == 5:
+        sair = opcao
+    else:
+        print('Voce digitou {}\nOPÇÃO INVALIDA'.format(opcao))
