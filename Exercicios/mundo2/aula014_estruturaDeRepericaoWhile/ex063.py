f4ddbd3d521a297a22567139c0f5bf76b05743c9
Marcos <marcosxavier03@gmail.com>
2021-11-03 23:00:31 -0300

    exercicio 63 concluido

--- Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex063.py
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex063.py
@@ -1,2 +1,15 @@
 # Exercício Python 063: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros
 # elementos de uma Sequência de Fibonacci.
+
+n = int(input('Digite a quantidade de termos que deseja ver na sequencia de Fibonacci: '))
+t1 = 0
+t2 = 1
+print('{} - {}'.format(t1, t2), end=' - ')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(t3, end=' - ')
+    cont += 1
+    t1 = t2
+    t2 = t3
+print('FIM')
