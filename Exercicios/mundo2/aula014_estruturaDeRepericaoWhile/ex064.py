85731270ea17b2be94f561c0cf6b1994994bf55c
Marcos <marcosxavier03@gmail.com>
2021-11-03 22:23:48 -0300

    exercicio 64 concluido

--- Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex064.py
+++ Exercicios/mundo2/aula014_estruturaDeRepericaoWhile/ex064.py
@@ -1,3 +1,10 @@
 # Exercício Python 064: Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o
 # usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a
 # soma entre eles (desconsiderando o flag).
+cont = quant = soma = 0
+cont = int(input('Digite um número [999 para parar]: '))
+while cont != 999:
+    quant += 1
+    soma += cont
+    cont = int(input('Digite um número [999 para parar]: '))
+print('A soma entre os {} digitados é de {}'.format(quant, soma))
