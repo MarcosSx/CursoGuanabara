92c155513aff105a8bfdd0a615b0fa3d7e02f2b2
Marcos <marcosxavier03@gmail.com>
2021-11-04 22:04:56 -0300

    exercicio 66 concluido

--- Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex066.py
+++ Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex066.py
@@ -1,4 +1,11 @@
 # Exercício Python 066: Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o
 # usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a
 # soma entre elas (desconsiderando o flag).
-#todo ler numeros inteiros, parar em 999
+cont = soma = 0
+while True:
+    n = int(input('Digite um numero [para parar digite 999]: '))
+    if n == 999:
+        break
+    cont += 1
+    soma += n
+print(f'Vode digitou {cont} e a soma entre eles é de {soma}')
