d5d882ec3c00b4d9a50e74776c56b4b1f1e871be
Marcos <marcosxavier03@gmail.com>
2021-11-04 22:41:49 -0300

    exercicio 68 concluido

--- Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex068.py
+++ Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex068.py
@@ -1,3 +1,30 @@
 # Exercício Python 068: Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o
 # jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
-#todo par ou impar o jogo so é interrompido quando o jogador perder, mostrar total de vitorias
+from random import randint
+cont = 0
+bot = randint(0, 10)
+while True:
+    n = int(input('Insira um numero: '))
+    escolha = str(input('Par ou ímpar? [P/I]: '))
+    if escolha not in 'PpIi':
+        escolha = str(input('Par ou ímpar? [P/I]: '))
+    soma = n + bot
+    if soma % 2 == 0:
+        if escolha in 'Pp':
+            print('Voce venceu')
+            print(f'O computador escolheu Ímpar e jogou {bot} e voce escolheu Par e jogou {n} e a soma é {soma}')
+            cont += 1
+        else:
+            print('Voce perdeu')
+            print(f'O computador escolheu Par e jogou {bot} e voce escolheu Ímpar e jogou {n} e a soma é {soma}')
+            break
+    else:
+        if escolha in 'Ii':
+            print('Voce venceu')
+            print(f'O computador escolheu Par e jogou {bot} e voce escolheu Ímpar e jogou {n} e a soma é {soma}')
+            cont += 1
+        else:
+            print('Voce perdeu')
+            print(f'O computador escolheu Ímpar e jogou {bot} e voce escolheu Par e jogou {n} e a soma é {soma}')
+            break
+print(f'FIM DE JOGO\nVoce ganhou {cont} vezes')
