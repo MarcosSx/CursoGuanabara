78e0da18f7ffc656cdae0e2496209dd8f4b5c6b5
Marcos <marcosxavier03@gmail.com>
2021-11-05 01:20:59 -0300

    exercicio 70 concluido

--- Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex070.py
+++ Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex070.py
@@ -3,4 +3,30 @@
 # A) qual é o total gasto na compra.
 # B) quantos produtos custam mais de R$1000.
 # C) qual é o nome do produto mais barato.
-#todo ler nome e preço dos produtos, mostrar total gasto, produtos que valem mais de 1000 e produto mais barato
+total = quant = cont = 0
+loja = 'LOJA DO MARCÃO'
+print('-@-' * 15)
+print(f'{loja:^45}')
+print('-@-' * 15)
+while True:
+    print('-' * 50)
+    nome = str(input('Nome do produto: >>>'))
+    preco = float(input('Digite o valor do produto: R$'))
+    print('-' * 50)
+    total += preco
+    cont += 1
+    if preco > 1000:
+        quant += 1
+    if cont == 1:
+        barato = preco
+        produto = nome
+    if barato > preco:
+        barato = preco
+        produto = nome
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Deseja continuar [S/N]: ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+print(f'O total gasto é de R${total:.2f}, {quant} custaram mais de R$1000.00 e o produto mais barato foi {produto}')
+
