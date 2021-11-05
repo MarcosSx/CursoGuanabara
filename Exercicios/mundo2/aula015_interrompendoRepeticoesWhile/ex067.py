a61e2667d0e7d929b9901f55417afa48ddbd48c3
Marcos <marcosxavier03@gmail.com>
2021-11-04 22:16:27 -0300

    exercicio 67 concluido

--- Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex067.py
+++ Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex067.py
@@ -1,3 +1,9 @@
 # Exercício Python 067: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor
 # digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
-#todo mostrar a tabuada do valor que o usuario digitar e parar quando o numero for negativo
+while True:
+    print('#-#-' * 20)
+    t = int(input('Digite um numero para obter sua tabuada [para parar digite um numero negativo]: '))
+    if t < 0:
+        break
+    for n in range(0, 11):
+        print(f'{t} x {n:<2} = {t * n}')
