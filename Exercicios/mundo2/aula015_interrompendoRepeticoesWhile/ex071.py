b129b47bf8036292b7fffdb08f4c029a79a2ff00
Marcos <marcosxavier03@gmail.com>
2021-11-06 15:12:46 -0300

    exercicio 71 concluido

--- Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex071.py
+++ Exercicios/mundo2/aula015_interrompendoRepeticoesWhile/ex071.py
@@ -2,4 +2,26 @@
 # usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão
 # entregues.
 # OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
-#todo simular um caixa eletronico
+print('=' * 30)
+print('-{:^28}-'.format('Banco senta aqui'))
+print('=' * 30)
+saque = int(input('Qual valor deseja sacar: R$'))
+total = saque
+cedula = 50
+totCed = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totCed += 1
+    else:
+        if totCed > 0:
+            print(f'Total de {totCed} cedulas de R${cedula:.2f} ')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totCed = 0
+        if total == 0:
+            break
