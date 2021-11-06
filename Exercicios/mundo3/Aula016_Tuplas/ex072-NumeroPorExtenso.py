ca74510ab8f00a68a811c5d08c19fb096adcadbf
Marcos <marcosxavier03@gmail.com>
2021-11-06 18:00:51 -0300

    Adicionando exercicios da aula 16 TUPLAS

---
+++ Exercicios/mundo3/Aula016_Tuplas/ex072-NumeroPorExtenso.py
@@ -0,0 +1,3 @@
+#  Exercício Python 072: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de
+#  zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso
+# todo Passar para numero em extenso
