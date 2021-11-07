003b1e37057a01bf6a5ee395da848b8f9b97c918
Marcos <marcosxavier03@gmail.com>
2021-11-06 21:26:35 -0300

    exercicio 73 concluido

--- Exercicios/mundo3/Aula016_Tuplas/ex073-TuplasComTimesDeFutebol.py
+++ Exercicios/mundo3/Aula016_Tuplas/ex073-TuplasComTimesDeFutebol.py
@@ -4,4 +4,10 @@
 # b) Os últimos 4 colocados.
 # c) Times em ordem alfabética.
 # d) Em que posição está o time da Chapecoense.
-# todo
+times = 'Atlético-MG', 'Flamengo', 'Palmeiras', 'Bragantino', 'Fortaleza', 'Corinthians', 'Internacional', 'Fluminense',\
+        'Cuiabá', 'América-MG', 'Atlético-GO', 'Sao Paulo', 'Ceará SC', 'Atlético-PR', 'Santos', 'Bahia', 'Sport Recife',\
+        'Juventude', 'Grêmio', 'Chapecoense'
+print(f'Os cinco primeiros colocados são: {times[:5]}')
+print(f'Os quatro ultimos colocados são {times[16:]}')
+print(f'Times no Brasileirão 2021\n{sorted(times)}')
+print(f'A Chapecoense está na posição {times.index("Chapecoense") + 1}')
