d98040d6b9b654fc6bfc696f2b48382b9e2ec44b
Marcos <marcosxavier03@gmail.com>
2021-11-07 13:30:32 -0300

    exercicio 77 concluido

--- Exercicios/mundo3/Aula016_Tuplas/ex077-ContandoVogaisEmTupla.py
+++ Exercicios/mundo3/Aula016_Tuplas/ex077-ContandoVogaisEmTupla.py
@@ -1,3 +1,10 @@
 # Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você
 # deve mostrar, para cada palavra, quais são as suas vogais.
-# todo
+palavras = 'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'Etiam', 'tincidunt', 'eros', \
+           'dignissim', 'venenatis', 'purus', 'elit', 'lacinia', 'velit', 'vestibulum'
+
+for p in range(0, len(palavras)):
+    print(f'\nNa palavra {palavras[p]} temos:', end=' ')
+    for letras in palavras[p]:
+        if letras.upper() in 'AEIOU':
+            print(letras, end='')
