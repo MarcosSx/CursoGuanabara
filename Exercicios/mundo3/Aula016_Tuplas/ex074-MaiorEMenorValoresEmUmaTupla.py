bf698b573059c805e8d3a640993b6328d8c0d2b3
Marcos <marcosxavier03@gmail.com>
2021-11-06 21:34:41 -0300

    exercicio 74 concluido

--- Exercicios/mundo3/Aula016_Tuplas/ex074-MaiorEMenorValoresEmUmaTupla.py
+++ Exercicios/mundo3/Aula016_Tuplas/ex074-MaiorEMenorValoresEmUmaTupla.py
@@ -1,3 +1,6 @@
 # Exercício Python 074: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso,
 # mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
-# todo
+from random import randint
+
+numero = randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100)
+print(f'Os numeros gerados foram {numero}, o menor valor é {min(numero)} e o maior valor é {max(numero)}')
