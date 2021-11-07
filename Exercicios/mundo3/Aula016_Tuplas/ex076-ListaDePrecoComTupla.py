3602a26a781402a176d450a0d2008c5f5da27c11
Marcos <marcosxavier03@gmail.com>
2021-11-07 11:49:43 -0300

    exercicio 76 concluido

--- Exercicios/mundo3/Aula016_Tuplas/ex076-ListaDePrecoComTupla.py
+++ Exercicios/mundo3/Aula016_Tuplas/ex076-ListaDePrecoComTupla.py
@@ -1,3 +1,12 @@
 # Exercício Python 076: Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na
 # sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular
-# todo
+produtos = 'Arroz', 10, 'Feijao', 5, 'Carne', 20, 'Manteiga', 3, 'Batata', 5, 'Frango', 19.9
+print('_' * 50)
+print(f'{"LISTA DE PREÇOS":^50}')
+print('_' * 50)
+for p in range(len(produtos)):
+    if p % 2 == 0:
+        print(f'{produtos[p]:.<40}', end='')
+    else:
+        print(f'R${produtos[p]:>8.2f}')
+print('_' * 50)
