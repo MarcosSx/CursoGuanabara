1c8bb92ea5d96c7a063a87adf89e1003919a21cd
Marcos <marcosxavier03@gmail.com>
2021-11-08 22:38:43 -0300

    exercicio 78 concluido

--- Exercicios/mundo3/Aula017_Listas/ex078-MaiorEMenorValorEmUmaLista.py
+++ Exercicios/mundo3/Aula017_Listas/ex078-MaiorEMenorValorEmUmaLista.py
@@ -1,3 +1,15 @@
 # Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual
 # foi o maior e o menor valor digitado e as suas respectivas posições na lista.
-# TODO
+numeros = list()
+maior = menor = 0
+for p in range(0, 5):
+    numeros.append(int(input(f'Digite um numero para a posição {p}: ')))
+print(f'Lista de numeros: {numeros}')
+print(f'O maior valor é {max(numeros)} e esta na posição: ', end='')
+for i, val in enumerate(numeros):
+    if val == max(numeros):
+        print(f'{i}...', end='')
+print(f'\nO menor valor é {min(numeros)} e esta na posição: ', end='')
+for i, val in enumerate(numeros):
+    if val == min(numeros):
+        print(f'{i}...', end='')
