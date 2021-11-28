f5ae8bdca231f08da3091382c531c1b0707d6f3d
Marcos <marcosxavier03@gmail.com>
2021-11-28 03:56:28 -0300

    exercicio ex081-ExtraindoDadosDeUmaLista finalizado

--- Exercicios/mundo3/Aula017_Listas/ex081-ExtraindoDadosDeUmaLista.py
+++ Exercicios/mundo3/Aula017_Listas/ex081-ExtraindoDadosDeUmaLista.py
@@ -2,4 +2,18 @@
 # A) Quantos números foram digitados.
 # B) A lista de valores, ordenada de forma decrescente.
 # C) Se o valor 5 foi digitado e está ou não na lista.
-# TODO
+
+lista = list()
+while True:
+    lista.append(int(input('Digite um numero: ')))
+    parar = ' '
+    while parar not in 'SN':
+        parar = input('Deseja continuar? [S/N] ').strip()[0].upper()
+
+    if parar in 'N':
+        break
+
+lista.sort(reverse=True)
+print(f'Voce digitou {len(lista)} numeros')
+print(f'Os numeros digitados em ordem decrescente são: {lista}')
+print(f'O numero 5 foi encontrado na lista' if lista.count(5) > 0 else f'O numero 5 não foi encontrado na lista')
