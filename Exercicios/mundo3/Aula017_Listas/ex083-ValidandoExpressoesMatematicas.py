972dc4257c06d7e2c130be93c0bc3d675cef3922
Marcos <marcosxavier03@gmail.com>
2021-11-28 14:28:53 -0300

    exercicio ex083-ValidandoExpressoesMatematicas finalizado

--- Exercicios/mundo3/Aula017_Listas/ex083-ValidandoExpressoesMatematicas.py
+++ Exercicios/mundo3/Aula017_Listas/ex083-ValidandoExpressoesMatematicas.py
@@ -1,3 +1,18 @@
 # Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu
 # aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
-# TODO
+
+lista_expressao = list()
+exp = str(input('Digite uma expressão: '))
+for p in exp:
+    if '(' in p:
+        lista_expressao.append(p)
+    elif ')' in p:
+        if len(lista_expressao) > 0:
+            lista_expressao.pop()
+        else:
+            lista_expressao.append(p)
+            break
+if len(lista_expressao) == 0:
+    print('Expressao valida')
+else:
+    print('Expressão invalida')
