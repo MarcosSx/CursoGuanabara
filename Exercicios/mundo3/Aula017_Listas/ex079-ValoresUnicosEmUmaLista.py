9598ee54334e08a2d7dafabb6ee78c881c161b32
Marcos <marcosxavier03@gmail.com>
2021-11-28 01:57:31 -0300

    exercicio ex079-ValoresUnicosEmUmaLista finalizado

--- Exercicios/mundo3/Aula017_Listas/ex079-ValoresUnicosEmUmaLista.py
+++ Exercicios/mundo3/Aula017_Listas/ex079-ValoresUnicosEmUmaLista.py
@@ -1,4 +1,21 @@
 # Exercício Python 079: Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma
 # lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos
 # digitados, em ordem crescente.
-# TODO
+
+lista = list()
+
+while True:
+    numero = int(input('Digite um numero: '))
+
+    if numero in lista:
+        print(f'O numero {numero} já foi digitado')
+    else:
+        lista.append(numero)
+
+    parar = ' '
+    while parar not in 'SN':
+        parar = input('Deseja parar [S/N]: ').upper().strip()[0]
+    if parar in 'S':
+        break
+
+print(sorted(lista))
