17e17ae811ac8268141750e45a2af068154f88a9
Marcos <marcosxavier03@gmail.com>
2021-12-11 16:34:37 -0300

    correçao no exercicio 32

--- Exercicios/mundo1/aula010_condicoes/ex032-AnoBissexto.py
+++ Exercicios/mundo1/aula010_condicoes/ex032-AnoBissexto.py
@@ -3,7 +3,8 @@ import datetime
 ano = int(input('Digita um um ano ai karay:\n'))
 esteAno = datetime.date.today().year
 print('este ano é bissesto' if esteAno % 4 == 0 and esteAno % 100 != 0 and esteAno % 400 != 0 else 'este ano não é bissesto')
-if ano % 4 == 0 and ano % 100 != 0 and ano % 400 != 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('Ano Bissesto parça')
 else:
     print('Não bissesto mane')
+
