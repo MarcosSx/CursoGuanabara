4947def37b0fd9c3c9d21b8b9333ef6affc3eea4
Marcos <marcosxavier03@gmail.com>
2021-12-21 11:58:14 -0300

    exercicio ex090-DicionarioEmPython finalizado

---
+++ Exercicios/mundo3/Aula019_Dicionarios/ex090-DicionarioEmPython.py
@@ -0,0 +1,16 @@
+# Exercício Python 90: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+# No final, mostre o conteúdo da estrutura na tela.
+
+aluno = {'Nome': str(input('Nome: '))}
+aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+if aluno['Média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+elif 5 <= aluno['Média'] < 7:
+    aluno['Situação'] = 'Em recuperação'
+else:
+    aluno['Situação'] = 'Reprovado'
+print()
+for k, v in aluno.items():
+    print(f'{k} é igual a {v}')
+
+
