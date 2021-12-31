3c9c48e7c4f2ac63b14ca96e116ec09a4b19c8ea
Marcos <marcosxavier03@gmail.com>
2021-12-31 19:12:05 -0300

    ex114-SiteEstaAcessicel finalizado

--- Exercicios/mundo3/Aula023_TratamentoDeErrosEExcecoes/ex114-SiteEstaAcessicel.py
+++ Exercicios/mundo3/Aula023_TratamentoDeErrosEExcecoes/ex114-SiteEstaAcessicel.py
@@ -1,2 +1,11 @@
 # Exercício Python 114: Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
 
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib:
+    print('deu erro')
+else:
+    print('tudo certo')
