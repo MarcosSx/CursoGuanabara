1588ab564bd2610e082d41b5d6a6ceb984c36af8
Marcos <marcosxavier03@gmail.com>
2021-12-31 19:02:58 -0300

    ex113-FuncoesAprofundadasEmPython finalizado

--- Exercicios/mundo3/Aula023_TratamentoDeErrosEExcecoes/ex113-FuncoesAprofundadasEmPython.py
+++ Exercicios/mundo3/Aula023_TratamentoDeErrosEExcecoes/ex113-FuncoesAprofundadasEmPython.py
@@ -1,3 +1,30 @@
 # Exercício Python 113: Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade da
 # digitação de um número de tipo inválido. Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+            continue
+        else:
+            return n
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = str(input(msg)).replace(',', '.')
+            num = float(n)
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+            continue
+        else:
+            return num
+
+
+# num = leiaInt('Digite um valor: ')
+numero = leiaFloat('Digite um valor: ')
+# print(f'O valor digitado foi {num}')
+print(f'O valor digitado foi {numero}')
