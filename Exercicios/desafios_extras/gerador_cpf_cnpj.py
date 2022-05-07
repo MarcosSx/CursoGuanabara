d479065142420392ab8789c59fced2d3269cb6d3
Marcos <marcosxavier03@gmail.com>
2022-05-07 19:57:36 -0300

    refatornado geradores de cpf e cnpj

--- Exercicios/desafios_extras/gerador_cpf_cnpj.py
+++ Exercicios/desafios_extras/gerador_cpf_cnpj.py
@@ -1,61 +1,57 @@
 from random import randint
 
 
-def cpf_generator(pontuado=True):
-    cpf = list()
-    # gera 9 numeros aleatorios e adiciona na lista cpf xxx.xxx.xxx
-    for num in range(0, 9):
-        cpf.append(randint(0, 9))
-    # gerar os dois ultimos digitos e adiciona ao cpf xxx.xxx.xxx-xx
-    for i in range(0, 2):
-        quant = len(cpf) + 1
-        soma = 0
-        for n in cpf:
-            soma += n * quant
-            quant -= 1
-        if soma % 11 < 2:
-            cpf.append(0)
-        else:
-            cpf.append(11 - (soma % 11))
-
-    if pontuado:
-        return '%s%s%s.%s%s%s.%s%s%s-%s%s' % tuple(cpf)
-    else:
-        return '%s%s%s%s%s%s%s%s%s%s%s' % tuple(cpf)
-
-
-def cnpj_generator(pontuado=True):
-    cnpj = list()
-    # gera 12 numeros aleatorios e adiciona na lista cnpj xx.xxx.xxx/xxxx
-    for i in range(0, 12):
-        if len(cnpj) < 8:
-            cnpj.append(randint(0, 9))
-        elif len(cnpj) < 11:
-            cnpj.append(0)
-        else:
-            cnpj.append(1)
-    # gera os dois ultimos digitos e adiciona ao cnpj xx.xxx.xxx.xxx/xxxx-xx
-    for i in range(2):
-        contb1 = len(cnpj) - 7
-        contb2 = 9
-        soma = 0
-        for n in range(len(cnpj)):
-            if n < len(cnpj) - 8:
-                soma += cnpj[n] * contb1
-                contb1 -= 1
+def cpf_cnpj_numbers_generator(type):
+    generic_cpf_cnpj = []
+    if type.upper() == 'CPF':
+        for num in range(0, 9):
+            generic_cpf_cnpj.append(randint(0, 9))
+    elif type.upper() == 'CNPJ':
+        for i in range(0, 12):
+            if len(generic_cpf_cnpj) < 8:
+                generic_cpf_cnpj.append(randint(0, 9))
+            elif len(generic_cpf_cnpj) < 11:
+                generic_cpf_cnpj.append(0)
             else:
-                soma += cnpj[n] * contb2
-                contb2 -= 1
-        if soma % 11 < 2:
-            cnpj.append(0)
+                generic_cpf_cnpj.append(1)
+    return generic_cpf_cnpj
+
+
+def validator_digit_generator(numbers):
+    sum_digits = 0
+    for i, j in enumerate(reversed(numbers), start=2):
+        if len(numbers) < 12:
+            sum_digits += i * j
         else:
-            cnpj.append(11 - (soma % 11))
+            if i <= 9:
+                sum_digits += i * j
+            else:
+                sum_digits += (i - 8) * j
+    cpf_cnpj_dv = abs(sum_digits % 11 - 11)
+    if sum_digits % 11 < 2:
+        cpf_cnpj_dv = 0
+    return cpf_cnpj_dv
 
-    if pontuado:
-        return '%d%d.%d%d%d.%d%d%d/%d%d%d%d-%d%d' % tuple(cnpj)
+
+def formatter(doc, formatted=True):
+    formatted_doc = ''
+    if formatted:
+        if len(doc) <= 12:
+            formatted_doc = '%s%s%s.%s%s%s.%s%s%s-%s%s' % tuple(doc)
+        else:
+            formatted_doc = '%d%d.%d%d%d.%d%d%d/%d%d%d%d-%d%d' % tuple(doc)
     else:
-        return '%d%d%d%d%d%d%d%d%d%d%d%d%d%d' % tuple(cnpj)
+        for i in doc:
+            formatted_doc += str(i)
+    return formatted_doc
 
 
-print(cpf_generator(False))
-print(cnpj_generator(False))
+def cpf_cnpj_generator(type, formatted=False):
+    cpf_cnpj = cpf_cnpj_numbers_generator(type.upper())
+    cpf_cnpj.append(validator_digit_generator(cpf_cnpj))
+    cpf_cnpj.append(validator_digit_generator(cpf_cnpj))
+    return formatter(cpf_cnpj, formatted)
+
+
+print(cpf_cnpj_generator('cpf', True))
+print(cpf_cnpj_generator('cnpj', True))
